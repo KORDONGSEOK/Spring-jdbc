3876b48b78726030b8bede54454dca7be1d06708
sweatone <chas0302@naver.com>
2023-07-07 14:47:57 +0900

    select MemberRepositoryV0 (DB2&PostgreSQL)

--- src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -1,11 +1,11 @@
 package hello.jdbc.connection;
 
 public abstract class ConnectionConst {
-    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
-    public static final String USERNAME = "sa";
-    public static final String PASSWORD = "";
+//    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+//    public static final String USERNAME = "sa";
+//    public static final String PASSWORD = "";
 
-//    public static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-//    public static final String USERNAME = "postgres";
-//    public static final String PASSWORD = "1234";
+    public static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "1234";
 }
